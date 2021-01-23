d4b43446c11a04fab6401930c9cdf403384013bb
Chery Sun <alittlemoyu@gmail.com>
2021-01-23 18:52:17 +0800

    添加题库

--- AnswerGetter.py
+++ AnswerGetter.py
@@ -3,7 +3,7 @@ def answerGetter(questionNolist):
     for questionKind in questionNolist:
         for questionNo in questionKind[0]:
             QuestionContent = open(
-                'C:\\Users\\holyCRAP\\OneDrive\\Python\\Database\\' + questionKind[1] + '\\' + str(questionNo) + '.txt',
+                'C:\\Users\\holyCRAP\\OneDrive\\Python\\AutoTestSystem\\Database\\' + questionKind[1] + '\\' + str(questionNo) + '.txt',
                 'r')
             QuestionAnswer[0].append(QuestionContent.readlines()[-1])
     return QuestionAnswer
